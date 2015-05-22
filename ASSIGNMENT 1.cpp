23a45333e47182e1f375c77e9683b5caf8b2d5a1
benjamin02 <benjamin6192@gmail.com>
2015-05-22 23:59:07 +0100

    code for c++
    source code for c++

---
+++ ASSIGNMENT 1.cpp
@@ -0,0 +1,28 @@
+// NAME:ENULANE BENJAMIN BESSONG
+// MATRIC NO:45303119FJ
+// COMPUTER SCIENCE
+
+
+
+#include<iostream>
+using namespace std
+
+;
+int main ()
+{
+	int firstinteger;
+	int secondinteger;
+	
+	cout << "enter integer: ";
+	cin >> firstinteger;
+	
+	
+	secondinteger = firstinteger/ 2;
+	if (firstinteger % secondinteger ==0)
+	
+	cout << firstinteger << "is an even number\n" ;
+	
+	else
+	cout << firstinteger << "is an odd number\n";
+	return 0;
+}
---
+++ ASSIGNMENT 4.cpp
@@ -0,0 +1,49 @@
+// NAME:ENULANE BENJAMIN BESSONG
+// MATRIC NO:45303119FJ
+// COMPUTER SCIENCE
+
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "*********\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*********\n";
+	     cout << "   ** \n"
+	         << " *    * \n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << " *    *\n"
+	         << "   **  \n";
+	     cout << "  *   \n"
+	          << " ***  \n"
+	          << "***** \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n";
+	     cout << "    *   \n"
+	          << "   * *    \n"
+	         << " *     *  \n"
+	        << "*       *\n"
+	        << " *     * \n"
+	        << "   * *   \n"
+	        << "    *    \n";
+	        
+	        return 0;
+}
